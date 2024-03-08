9858726db74289f13b9795baef4bd53cc87b85d9
GagoChad <gagika57@gmail.com>
2024-03-08 23:44:27 +0500

    Add Hover for Buttons

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<button
-		class="bg-blue-500 mt-10 ml-6 w-80 rounded-sm text-white text-center py-2 border border-black"
+		class="bg-blue-500 hover:bg-blue-600 transition ease-linear delay-100 mt-10 ml-6 w-80 rounded-sm text-white text-center py-2 border border-black"
 	>
 		Добавить
 	</button>
