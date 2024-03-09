759b0fc5886a9d49262fca6388f8b9405f8b119b
GagoChad <gagika57@gmail.com>
2024-03-09 22:36:04 +0500

    Fix some UI bugs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,10 @@ import SaveButton from './SaveButton.vue'
 			</div>
 
 			<div class="col-span-12 pt-12 mr-12">
-				<table class="w-full table-auto h-auto overflow-scroll">
+				<div>
+					
+				</div>
+				<table class="w-full table-auto overflow-scroll max-h-32">
 					<thead>
 						<tr>
 							<th class="px-4 text-blue-600 py-2 font-light text-left text-xl">
@@ -39,33 +42,13 @@ import SaveButton from './SaveButton.vue'
 								1100.00
 							</td>
 						</tr>
-						<tr>
-							<td class="px-4 py-3 font-light border-gray-300">
-								Кости для ухи
-							</td>
-							<td class="px-4 py-2 text-right border-gray-300">
-								<input
-									value="20 шт."
-									class="w-12 bg-transparent font-light text-center"
-								/>
-							</td>
-							<td class="px-4 font-light py-2 text-right">2000.00</td>
-						</tr>
-						<tr>
-							<td class="px-4 font-light py-3">Вода для щщей</td>
-							<td class="px-4 py-2 text-right">
-								<input
-									value="1 шт."
-									class="w-12 bg-transparent font-light text-center"
-								/>
-							</td>
-							<td class="px-4 font-light py-2 text-right">300.00</td>
-						</tr>
+						
 					</tbody>
 				</table>
 			</div>
 
-			<div class="row-span-2 col-span-12">
+
+			<div class="row-span-2 col-span-12 mt-12">
 				<div class="rounded-lg p-4">
 					<h2 class="text-lg text-right mr-12 mb-2">Итого: 3400</h2>
 					<SaveButton />
